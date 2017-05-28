86535152d42f1705b9d4f3c3c0b4d59537eccb59
filiperfernandes <filipe.ramalhofernandes@gmail.com>
2017-05-28 01:57:00 +0100

    Bug fix of list methods

--- reads_app/views.py
+++ reads_app/views.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
 
 # Create your views here.
 
-import urllib
 
 from django.http import HttpResponse, JsonResponse
 from django.utils import timezone
@@ -20,21 +18,16 @@ def index(request):
 def location_info(request, location_id):
     l = Location.objects.get(id=location_id)
 
-    return JsonResponse({"Latitude": l.lat, "Longitude":l.lon, "Name": l.name})
+    return JsonResponse({"Latitude": l.lat, "Longitude": l.lon, "Name": l.name})
 
 
 def list_locations(request):
-
-    list = Location.objects.all()
+    list_loc = Location.objects.all()
 
     response_data = []
-    temp_dict = {'Latitude': 0.0, 'Longitude': 0.0, 'Name': "default"}
 
-    for i in range(len(list)):
-        temp_dict['Latitude'] = list[i].lat
-        temp_dict['Longitude'] = list[i].lon
-        temp_dict['Name'] = list[i].name
-        response_data.append(temp_dict)
+    for i in range(len(list_loc)):
+        response_data.append({'Latitude': list_loc[i].lat, 'Longitude': list_loc[i].lon, 'Name': list_loc[i].name})
 
     return JsonResponse(response_data, safe=False)
 
@@ -42,26 +35,20 @@ def list_locations(request):
 def notification_info(request, notification_id):
     n = Notification.objects.get(id=notification_id)
 
-    return JsonResponse({"LocationID": n.location.id, "Description": n.description, "Risk": n.risk, "Source": n.source, "Date": n.pub_date})
+    return JsonResponse({"LocationID": n.location.id, "Description": n.description, "Risk": n.risk, "Source": n.source,
+                         "Date": n.pub_date})
 
 
 def list_notifications(request):
-    list = Notification.objects.all()
+    list_n = Notification.objects.all()
 
     response_data = []
-    temp_dict = {"LocationID": 0, "Description": "empty", "Risk": 0, "Source": "john doe", "Date": timezone.now()}
 
-    for i in range(len(list)):
-        temp_dict['LocationID'] = list[i].location.id
-        temp_dict['Description'] = list[i].description
-        temp_dict['Risk'] = list[i].risk
-        temp_dict['Source'] = list[i].source
-        temp_dict['Date'] = list[i].pub_date
-        response_data.append(temp_dict)
+    for i in range(len(list_n)):
+        response_data.append({"LocationID": list_n[i].location.id, "Description": list_n[i].description,
+                              "Risk": list_n[i].risk, "Source": list_n[i].source, "Date": list_n[i].pub_date})
 
     return JsonResponse(response_data, safe=False)
-
-
 
 # To get the info
 # import json, urllib.request
