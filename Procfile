b5a293dab3d67052488b092b7c6d31168f18b0fe
filiperfernandes <filipe.ramalhofernandes@gmail.com>
2017-05-29 15:41:35 +0100

    Add heroku support

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn reads_server.wsgi:application --log-file -
--- reads_server/settings.py
+++ reads_server/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +28,7 @@ SECRET_KEY = '=mk2f**b#a17u0slt57^e0h26iu@smjqzruoey-g0%i-9*@+$y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','0.0.0.0']
 
 
 # Application definition
@@ -84,6 +87,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
@@ -123,3 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
